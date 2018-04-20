34a6434edfcaf472366942ec9b28495dbc97e6d7
Tyler Peterson <legendofpie@gmail.com>
2018-04-19 21:40:21 -0400

    Tons o' shit

---
+++ PhotonPractice/Assets/Cieric's Test Shit/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    new Rigidbody rigidbody;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            rigidbody.AddForce(0, 0, 10);
+        }
+    }
+}
