e88781335511a64d75362b9a5943f107c425c5b2
Tyler Peterson <legendofpie@gmail.com>
2018-04-19 22:25:10 -0400

    Added a basic player script.

--- PhotonPractice/Assets/Cieric's Test Shit/Player.cs
+++ PhotonPractice/Assets/Cieric's Test Shit/Player.cs
@@ -7,6 +7,12 @@ public class Player : MonoBehaviour {
     [SerializeField]
     new Rigidbody rigidbody;
 
+    [SerializeField]
+    new Transform transform;
+
+    [SerializeField]
+    float CapSpeed = 1;
+
 	void Start ()
     {
 		
@@ -19,9 +25,25 @@ public class Player : MonoBehaviour {
 
     void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            rigidbody.AddForce(0, 0, 10);
+            rigidbody.AddRelativeForce(0, 0, 10);
+            if(rigidbody.velocity.magnitude < CapSpeed)
+                rigidbody.velocity = rigidbody.velocity.normalized * CapSpeed;
         }
+        else if(rigidbody.velocity.magnitude < CapSpeed)
+        {
+            rigidbody.velocity = new Vector3(0,rigidbody.velocity.y,0);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            Vector3 rot = transform.rotation.eulerAngles;
+            if(Input.GetKey(KeyCode.A))
+                rot.y -= 1;
+            if (Input.GetKey(KeyCode.D))
+                rot.y += 1;
+            transform.rotation = Quaternion.Euler(rot);
+        }
+
     }
 }
